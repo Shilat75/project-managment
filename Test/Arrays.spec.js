c0e96b871064d4c1ceda52a3f83ae5dc75dc3634
Shilat75 <93251948+Shilat75@users.noreply.github.com>
2023-04-13 23:14:53 +0300

    xxx

---
+++ Test/Arrays.spec.js
@@ -0,0 +1,12 @@
+import { expect } from "chai";
+
+import (expect)
+
+describe('Arrays',()=>{
+
+    describe('#sort',()=>{
+        it('Sorting names array ',()=>{
+           /////////passs
+        })
+    })
+})
---
+++ src/index.js
@@ -0,0 +1,14 @@
+import express from 'express'
+
+const port= process.env.PORT||3000;
+const app = express();
+
+app.get('/',(req,res)=>{
+    console.log("A new request has arrived to index.js");
+    res.send("Hello from thegg server main page");
+}
+);
+
+app.listen(port, ()=>{
+    console.log("Server is up and running at port: " + port);
+});
